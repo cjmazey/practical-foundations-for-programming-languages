0c3c6f292b040664fef5d44a8d1a4ea2080e6a84
Christopher Mazey <cjmazey@gmail.com>
2015-10-13 16:15:00 -0400

    structural induction

--- 01/foo.lean
+++ 01/foo.lean
@@ -10,4 +10,13 @@ namespace exp
   definition X := var 0
   definition exp_ex1 : exp := plus (num 2) (times (num 3) X)
   definition exp_ex2 : exp := plus (num 2) (times (num 3) (num 4))
+
+  theorem structural_induction :
+      ∀ P : exp → Prop,
+      (∀ n : ℕ, P (var n)) →
+      (∀ n : ℕ, P (num n)) →
+      (∀ a₁ a₂ : exp, P a₁ → P a₂ → P (plus a₁ a₂)) →
+      (∀ a₁ a₂ : exp, P a₁ → P a₂ → P (times a₁ a₂)) →
+      ∀ a : exp, P a :=
+    @exp.rec
 end exp
