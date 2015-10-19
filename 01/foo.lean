fa737f99a7f3d421486f954c9ec8e02653d10616
Christopher Mazey <cjmazey@gmail.com>
2015-10-19 04:47:17 -0400

    task 2.3

--- 01/foo.lean
+++ 01/foo.lean
@@ -20,3 +20,54 @@ namespace exp
       ∀ a : exp, P a :=
     @exp.rec
 end exp
+
+namespace shuffling
+
+  inductive card : Type :=
+  | h : card
+  | s : card
+  | c : card
+  | d : card
+
+  open card
+
+  inductive stack : Type :=
+  | nil : stack
+  | cons : card → stack → stack
+
+  open stack
+
+  inductive unshuffle : stack → stack → stack → Prop :=
+  | unz : unshuffle nil nil nil
+  | unr : ∀ {s₁ s₂ s₃ : stack} (c : card), unshuffle s₁ s₂ s₃ →
+              unshuffle (cons c s₁) s₂ (cons c s₃)
+  | unl : ∀ {s₁ s₂ s₃ : stack} (c : card), unshuffle s₁ s₂ s₃ →
+              unshuffle (cons c s₁) (cons c s₂) s₃
+
+  open unshuffle
+
+  -- Task 2.1
+  example : unshuffle (cons h (cons s (cons s (cons d nil))))
+                      (cons s (cons d nil))
+                      (cons h (cons s nil)) :=
+  unr h (unl s (unr s (unl d unz)))
+
+  -- Task 2.2
+  example : unshuffle (cons h (cons s (cons s (cons d nil))))
+                      (cons s (cons d nil))
+                      (cons h (cons s nil)) :=
+  unr h (unr s (unl s (unl d unz)))
+
+  -- Task 2.3
+  example : ∀ s₁, ∃ s₂, ∃ s₃, unshuffle s₁ s₂ s₃ :=
+  λ s₁,
+    stack.induction_on s₁
+    (exists.intro nil (exists.intro nil unz))
+    (take c : card,
+     take t₁ : stack,
+     take H : ∃ t₂, ∃ t₃, unshuffle t₁ t₂ t₃,
+     obtain t₂ H', from H,
+     obtain t₃ H'', from H',
+     exists.intro (cons c t₂) (exists.intro t₃ (unl c H'')))
+
+end shuffling
